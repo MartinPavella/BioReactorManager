ad9d06c0b5d778cdcc2c8cb093246e95dd0f662b
Martin Pavella <pavella.martin@icloud.com>
2025-08-09 14:19:12 +0200

    Update mixing cycle.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 import time
+from dataclasses import dataclass
+from datetime import datetime
 
-from datetime import datetime, timedelta
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from dataclasses import dataclass
 from pydantic import BaseModel
+
 import mqtt_manager
+
 app = FastAPI()
 
 # Enable CORS for frontend communication
@@ -22,7 +24,6 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-
 @dataclass
 class Layer:
     id_: int
@@ -30,23 +31,24 @@ class Layer:
     light_state: bool
     valve_state: bool
 
+
 layers: list[Layer] = [Layer(i, f'layer_{5 - i}', False, False) for i in range(5)]  # Current state of the layers.
 pump_state = False
 pump_perctentage = 50
 light_cycle_on = False
-mixiing_cycle_on = False
+mixing_cycle_on = False
+
 
 @app.get("/get-state")
 def get_state():
     return [
-        {"id_": l.id_, "name": l.name, "light_state": l.light_state, "valve_state": l.valve_state,} for l in layers
+        {"id_": l.id_, "name": l.name, "light_state": l.light_state, "valve_state": l.valve_state, } for l in layers
     ]
-
-
 
 
 class HarvestMeta(BaseModel):
     duration: int
+
 
 @app.post('/harvest-layer/{id_}')
 def harvest_layer(id_: int, harvest_meta: HarvestMeta):
@@ -70,7 +72,7 @@ def switch_light(id_: int):
     else:
         mqtt_manager.light_off(id_)
 
-    return {"id_" : id_, "new_light_state": new_light_state}
+    return {"id_": id_, "new_light_state": new_light_state}
 
 
 @app.post('/timer-lights')
@@ -95,46 +97,42 @@ def timer_lights():
             for id_ in range(5):
                 mqtt_manager.light_off(id_)
 
-        time.sleep(0.1)  # Check once every 10 seconds.
-
-
-
-@app.post('/timer-mixiing')
-def timer_mixiing():
-
-    global mixiing_cycle_on
-    if mixiing_cycle_on:
-        return  # Only 1 thread should handle this endpoint.
-    mixiing_cycle_on = True
-
-    # The lights are on from 6:00 to 21:00.
-    start_hour = 6
-    end_hour = 21
-    time_shift = -2  # The `datetime` for some reason shifts the time to a different time zone.
-    last_updated_time = datetime.now() - timedelta(hours=2)
-
-    while True:
-        if (start_hour + time_shift) <= datetime.now().hour < (end_hour + time_shift):
-            for id_ in range(5):
-                mqtt_manager.light_on(id_)
-
-            if (datetime.now() - last_updated_time).total_seconds()/60 >= 5:
-                mqtt_manager.open_valve(0)
-                time.sleep(0.5)
-                mqtt_manager.start_pump(80)
-                time.sleep(7.32)
-                mqtt_manager.stop_pump()
-                time.sleep(0.5)
-                mqtt_manager.close_valve(0)
-
-                last_updated_time = datetime.now()
-
-        else:
-            for id_ in range(5):
-                mqtt_manager.light_off(id_)
-
         time.sleep(10)  # Check once every 10 seconds.
 
+
+@app.post('/timer-mixing')
+def timer_mixing():
+    global mixing_cycle_on
+    if mixing_cycle_on:
+        return  # Only 1 thread should handle this endpoint.
+    mixing_cycle_on = True
+
+    layer_mixing_time_seconds = 7
+    time_to_fill_pipe_to_layers = [
+        0, 0, 0, 0, 3  # TODO Just 3 seconds for the final layer for now.
+    ]
+    layer_mixing_powers = [
+        63, 66, 69, 72, 75  # TODO Measure precise values.
+    ]
+
+    mixing_period_minutes = 5
+    last_updated_time = datetime.now()
+    while True:
+        if (datetime.now() - last_updated_time).total_seconds() / 60 >= mixing_period_minutes:
+            for layer_id, (mixing_power, time_to_fill_pipe) in enumerate(
+                    zip(layer_mixing_powers, time_to_fill_pipe_to_layers)
+            ):
+                mqtt_manager.open_valve(layer_id)
+                mqtt_manager.start_pump(mixing_power)
+                time.sleep(time_to_fill_pipe + layer_mixing_time_seconds)
+                mqtt_manager.stop_pump()
+                mqtt_manager.close_valve(layer_id)
+
+            mqtt_manager.stop_pump()  # Make sure the pump is not running.
+
+            last_updated_time = datetime.now()  # Record the mixing time.
+
+        time.sleep(10)  # Check once every 10 seconds.
 
 
 @app.post('/switch-valve/{id_}')
@@ -147,8 +145,7 @@ def switch_valve(id_: int):
     else:
         mqtt_manager.close_valve(id_)
 
-    return {"id_" : id_, "new_valve_state": new_valve_state}
-
+    return {"id_": id_, "new_valve_state": new_valve_state}
 
 
 @app.post('/pump-power-change/{value_}')
@@ -159,9 +156,7 @@ def pump_power_change(value_: int):
     if pump_state:
         mqtt_manager.start_pump(value_)
 
-
-    return {"value" : value_}
-
+    return {"value": value_}
 
 
 @app.post('/toggle-pump')
@@ -221,19 +216,19 @@ def harvest_all():
     return "Success"
 
 
-
-
 # Data model for receiving button messages
 class MessageRequest(BaseModel):
     message: str
+
 
 # Handle button clicks
 @app.post("/send-message/{button_id}")
 def send_message(button_id: int, request: MessageRequest):
     return {"response": f"Received '{request.message}' from Button {button_id}"}
 
-    
+
 if __name__ == "__main__":
     import uvicorn
+
     print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     uvicorn.run(app, host="0.0.0.0", port=8000)
