053b72f7c06cc3339865f198cbefaeeb64e0b901
Martin Pavella <pavella.martin@icloud.com>
2025-08-21 22:17:15 +0200

    Enable tailwind.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,18 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: true,
+        host: true, // listen on all network interfaces
+        port: 5173, // optional, just to be explicit
+        strictPort: true, // fail if the port is already in use
         allowedHosts: ['bioreactor.local'],
+        hmr: {
+            host: 'bioreactor.local', // HMR WebSocket should connect here
+            protocol: 'ws',            // use WebSocket
+        },
     },
     resolve: {
         alias: {
@@ -14,5 +20,4 @@ export default defineConfig({
             'chart.js': 'chart.js',
         },
     },
-})
-
+});
